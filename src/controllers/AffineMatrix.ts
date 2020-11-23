fd2c964d5065f9c4ff9bde796be9c324b0a40b6e
4031651 <4031651@gmail.com>
2020-11-23 12:28:11 +0200

    Rename labels in AffineMatrix controller.

--- src/controllers/AffineMatrix.ts
+++ src/controllers/AffineMatrix.ts
@@ -5,30 +5,30 @@ import { Matrix } from './Base';
 
 // eslint-disable-next-line import/prefer-default-export
 export class AffineMatrix extends Matrix {
-  ['X scaleX'] = 0;
-  ['X scaleY'] = 0;
-  ['Y scaleX'] = 0;
-  ['Y scaleY'] = 0;
+  ['Xx Factor (a)'] = 0;
+  ['Xy Factor (b)'] = 0;
+  ['Yx Factor (c)'] = 0;
+  ['Yy Factor (d)'] = 0;
   ['Translate X'] = 0;
   ['Translate Y'] = 0;
 
   constructor(name: string, m: IIFSMatrix) {
     super(name, m);
 
-    this['X scaleX'] = m.a;
-    this['X scaleY'] = m.b;
-    this['Y scaleX'] = m.c;
-    this['Y scaleY'] = m.d;
+    this['Xx Factor (a)'] = m.a;
+    this['Xy Factor (b)'] = m.b;
+    this['Yx Factor (c)'] = m.c;
+    this['Yy Factor (d)'] = m.d;
     this['Translate X'] = m.e;
     this['Translate Y'] = m.f;
   }
 
   toMatrix(): IIFSMatrix {
     return {
-      a: this['X scaleX'],
-      b: this['X scaleY'],
-      c: this['Y scaleX'],
-      d: this['Y scaleY'],
+      a: this['Xx Factor (a)'],
+      b: this['Xy Factor (b)'],
+      c: this['Yx Factor (c)'],
+      d: this['Yy Factor (d)'],
       e: this['Translate X'],
       f: this['Translate Y'],
       p: this.Probability,
@@ -39,10 +39,10 @@ export class AffineMatrix extends Matrix {
   addControllers(gui: dat.GUI) {
     this.addFolder(gui);
 
-    this.folder.add(this, 'X scaleX', -1, 1, 0.001).onFinishChange(this.handleChange);
-    this.folder.add(this, 'X scaleY', -1, 1, 0.001).onFinishChange(this.handleChange);
-    this.folder.add(this, 'Y scaleX', -1, 1, 0.001).onFinishChange(this.handleChange);
-    this.folder.add(this, 'Y scaleY', -1, 1, 0.001).onFinishChange(this.handleChange);
+    this.folder.add(this, 'Xx Factor (a)', -1, 1, 0.001).onFinishChange(this.handleChange);
+    this.folder.add(this, 'Xy Factor (b)', -1, 1, 0.001).onFinishChange(this.handleChange);
+    this.folder.add(this, 'Yx Factor (c)', -1, 1, 0.001).onFinishChange(this.handleChange);
+    this.folder.add(this, 'Yy Factor (d)', -1, 1, 0.001).onFinishChange(this.handleChange);
     this.folder.add(this, 'Translate X', -10, 10, 0.001).onFinishChange(this.handleChange);
     this.folder.add(this, 'Translate Y', -10, 10, 0.001).onFinishChange(this.handleChange);
     this.folder.add(this, 'Probability', 0, 1, 0.001).onFinishChange(this.handleChange);
